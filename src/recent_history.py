7f5626d2dae2beb8f7e1452d78b583051cdcbfa5
Adrian Forde <adrianforde4@gmail.com>
2019-05-09 23:17:31 +0100

    Adding functionality to get previous times

--- src/recent_history.py
+++ src/recent_history.py
@@ -11,6 +11,17 @@ def main():
         current_time = datetime.datetime.now()
         x = current_time.strftime('%Y-%m-%d %H:%M:%S')
         print(x)
+        thirty_mins_ago = current_time - datetime.timedelta(minutes = 30)
+        hour_ago = current_time - datetime.timedelta(hours = 1)
+        six_hours_ago = current_time - datetime.timedelta(hours = 6)
+        twelve_hours_ago = current_time - datetime.timedelta(hours = 12)
+
+        print(thirty_mins_ago.strftime('%Y-%m-%d %H:%M:%S'))
+        print(hour_ago.strftime('%Y-%m-%d %H:%M:%S'))
+        print(six_hours_ago.strftime('%Y-%m-%d %H:%M:%S'))
+        print(twelve_hours_ago.strftime('%Y-%m-%d %H:%M:%S'))
+        
+        break
         if current_time.second == 0:
             print(current_time)
             time.sleep(58)
