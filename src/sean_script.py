5a0d1b062c9375dae1bb3de442aa364f469ca723
harkinsean4 <s.harkin5@nuigalway.ie>
2019-05-12 18:25:08 +0100

    12/05/19 on this fine Sunday eveninig I worked thorugh organising the comments and replies in r/CryptoiCurrency subreddit

--- src/sean_script.py
+++ src/sean_script.py
@@ -1,18 +1,18 @@
-#This is a basic script to get used to usign praw
+# This is a basic script to get used to usign praw
 
 import praw
 
-reddit = praw.Reddit(client_id ='txRrFGuKPeDnSw',
-					client_secret = 'wV7ATmzBZlVnxSok7fIO8FBlyp0',
-					udername = 'harkinsean4',
-					password = '1508sh1998Red',
-					user_agent = 'CCPB v1.0')
-					
+reddit = praw.Reddit(client_id='txRrFGuKPeDnSw',
+                     client_secret='wV7ATmzBZlVnxSok7fIO8FBlyp0',
+                     username='harkinsean4',
+                     password='1508sh1998Red',
+                     user_agent='CCPB v1.0')
+
 crypto_subreddit = reddit.subreddit('CryptoCurrency')
 
-hot_crypto = crypto_subreddit.hot(limit = 5)
+hot_crypto = crypto_subreddit.hot(limit=5)
 
 for submission in hot_crypto:
-	if not submission.stickied:
-		#print(submission)					#returns the thread id, it is an object
-		print('Title: {}, ups: {}' .format(submission.title, submission.ups))
+    if not submission.stickied:
+        # print(submission)					#returns the thread id, it is an object
+        print('Title: {}, ups: {}' .format(submission.title, submission.ups))
--- src/sean_script_application.py
+++ src/sean_script_application.py
@@ -1,40 +1,64 @@
-#This is meant to be the more advanced scipt in using PRAW
+# This is meant to be the more advanced scipt in using PRAW
 
 import praw
+conversationDict = {}
 
-reddit = praw.Reddit(client_id ='txRrFGuKPeDnSw',
-					client_secret = 'wV7ATmzBZlVnxSok7fIO8FBlyp0',
-					udername = 'harkinsean4',
-					password = '1508sh1998Red',
-					user_agent = 'CCPB v1.0')
-					
-crypto_subreddit = reddit.subreddit('CryptoCurrency')	#this is the subreddit
+reddit = praw.Reddit(client_id='txRrFGuKPeDnSw',
+                     client_secret='wV7ATmzBZlVnxSok7fIO8FBlyp0',
+                     udername='harkinsean4',
+                     password='1508sh1998Red',
+                     user_agent='CCPB v1.0')
 
-hot_crypto = crypto_subreddit.hot(limit = 3)			#this is the retrieves a categroy of that subreddit
+crypto_subreddit = reddit.subreddit('CryptoCurrency')  # this is the subreddit
 
-for submission in hot_crypto:							#submissions are the subreddit threads, they are an object
-	if not submission.stickied:
-		print('Title: {}, ups: {}' .format(submission.title, submission.ups))	#title is an attribute of submission
-	
-		#get the comments on this thread/submission. The .list() is a PRAW functionality
-		#it returns a flattened list of all Comments. 
-		#This list may contain MoreComments instances if replace_more() was not called first.
-		submission.comments.replace_more(limit=0)		
-		
-		#replace_more is there because in Reddit there are so many replies 
-		#to comments that it you must 'load more comments' which is another 
-		#call to Reddit database, if this happens we don't bother loading them
-		
-		comment_list = submission.comments.list()
-		
-		for comment in comment_list:
-			print(20*'-')
-			print('Parent ID:', comment.parent())		#this returns in the thread ID, same as submission.id
-			print('Comment ID:', comment.id)
-			print(comment.body)		#comments are an object, this only prints 'top level comments, no actual replies
+hot_crypto = crypto_subreddit.hot(limit=5)  # this is the retrieves a categroy of that subreddit
 
-			#replies = comment.replies
-			#if len(replies) > 0:
-			#	for reply in replies:
-			#		print('		REPLY: {}' .format(reply.body))
-			
+for submission in hot_crypto:  # submissions are the subreddit threads, they are an object
+    if not submission.stickied and submission.ups > 500:
+        # title is an attribute of submission
+        print('Submission Title: {}, ups: {}' .format(submission.title, submission.ups))
+
+        # we want to get the comments on this thread/submission.
+        # The .list() is a PRAW functionality
+        # It returns a flattened list of all Comments.
+        # This list may contain MoreComments instances if replace_more() was not called first.
+
+        submission.comments.replace_more(limit=0)
+        # replace_more is there because in Reddit there are so many replies
+        # to comments that it you must 'load more comments' which is another
+        # call to Reddit database, if this happens we don't bother loading them
+
+        comment_list = submission.comments.list()[:100]
+
+        for comment in comment_list:
+            if comment not in conversationDict:
+                # store an array in the conversationDict based on the comment.id key
+                # this array contains the comment.body and another dictionary
+                conversationDict[comment.id] = [comment.body, {}]
+                if comment.parent() != submission.id:  # basically 'if this is not a top level comment' ->
+                    # then it must be a reply, therefore get the originanl comment parent id () as a string and
+                    # find the exisiting id of the top level comment
+                    # place the comment.id in position 1 of the array which is a nestedd conversationDict
+                    # and the value to that key is the comment ups and comment body
+                    parent = str(comment.parent())
+                    conversationDict[parent][1][comment.id] = [comment.ups, comment.body]
+
+        print('Done sorting comments')
+        print(20*'*')
+
+        for post_id in conversationDict:
+            message = conversationDict[post_id][0]  # gets the top level comment body
+            type(message)
+            # replies is a dictionary of replies to the comment/message
+            replies = conversationDict[post_id][1]
+            type(replies)
+            if len(replies) > 1:  # if replies has more than one array of replies
+                print(35*'_')
+                print('Original Message: {}'.format(message))
+
+                print('Replies:')
+                for reply in replies:  # loop thorugh the keys in replies dictionary
+                    print('\t--')
+                    # again, limiting to 200 characters for space-saving, not necessary
+                    print("\tupvotes: {} \n\treply: {}\n" .format(
+                        replies[reply][0], replies[reply][1][:200]))
